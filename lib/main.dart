f69ddb67393a19212aa0ad9fd1a06c7f983eb106
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2020-12-27 00:19:32 +0500

    change home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:github_profile_starter_kit/ui/screens/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
